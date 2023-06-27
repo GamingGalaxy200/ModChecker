258c111c739e6526dbbda84418b01dc815bbfe12
MRGalaxy <108481498+GamingGalaxy200@users.noreply.github.com>
2023-06-27 17:43:46 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# ModChecker
+
+## 1. Füge die aktuelle Liste von Mods auf der Blacklist in die "blacklist" Datei. Beispiel:  
+    // Blacklisted Weapon Mods  
+  	'1655786638', // Grimdark Armory  
+  	'1358695565', // LAGO Weapons Pack  
+  	'2099675634', // Additional Air Weapon  
+
+## 2. Füge die eigene Mod Liste in die "playermods" Datei ein. Beispiel: 
+  RHSUSAF	Steam	https://steamcommunity.com/sharedfiles/filedetails/?id=843577117    
+  RHSAFRF	Steam	https://steamcommunity.com/sharedfiles/filedetails/?id=843425103  
+  oder  
+  767380317: @Blastcore Edited (standalone version) 
+  450814997: Community Base Addons v3.15.8  
+  825179978: @Enhanced Soundscape 
+## 3. Führe "npm install"
+## 4. Führe "npm run start" aus.
+## 5. BEACHTE
+  Die Listen funktionieren nur wenn IDs angegeben wurden wie die Workshop Ids der Mods. 
+  Nur die Namen werden nicht funktionieren! 
+  Dieses kleine Script wurde von Chris für Signal Hill entworfen. 
---
+++ index.js
@@ -0,0 +1,43 @@
+import fs from "fs";
+
+const blacklistFilePath = './data/blacklist';
+const playerModsFilePath = './data/playermods';
+
+// Überprüfen, ob die Blacklist existiert. Wenn nicht, erstelle sie.
+if (!fs.existsSync(blacklistFilePath)) {
+  console.log(`Die Blacklist Datei existiert nicht. Bitte füge deine Werte ein.`);
+  fs.writeFileSync(blacklistFilePath, '');
+}
+// Überprüfen, ob die Playermods-Datei existiert. Wenn nicht, erstelle sie.
+if (!fs.existsSync(playerModsFilePath)) {
+  fs.writeFileSync(playerModsFilePath, '');
+}
+
+// Lesen der Blacklist Datei
+const inputData = fs.readFileSync(blacklistFilePath, 'utf8');
+// Extrahieren der IDs aus der Blacklist Datei
+const ids = inputData.match(/\d+/g);
+// Überprüfen, ob die IDs-Liste nicht leer ist
+if (!ids || ids.length === 0) {
+  console.log('Die Blacklist enthält keine IDs.');
+  process.exit(1);
+}
+
+// Lesen der Playermods-Datei
+const playerModsData = fs.readFileSync(playerModsFilePath, 'utf8');
+// Extrahieren der IDs aus der Playermods-Datei
+const existingIds = playerModsData.match(/\d+/g);
+// Überprüfen, ob es  IDs gibt, die in der Blacklist enthalten sind
+if (existingIds && existingIds.length > 0) {
+  const uniqueIds = existingIds.filter(id => ids.includes(id));
+  if (uniqueIds.length > 0) {
+    console.log(`Die folgenden IDs sind nicht erlaubt:\n${uniqueIds.join(',\n')}\n`);
+    for (let id2 of uniqueIds) {
+        if (id2 == "0") {
+            console.log(`Mod mit ID 0 erkannt, vorsicht!`)
+        }
+    }
+  } else {
+    console.log("Keine IDs von der Blacklist erkannt.")
+  }
+}
